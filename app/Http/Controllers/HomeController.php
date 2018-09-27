2201f5840242833060fa7a61b729a4ec4c0a2f45
Ajinkya <dube.ajinkya452@gmail.com>
2018-09-28 01:40:17 +0530

    Initial Commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Kreait\Firebase;
+ 
+use Kreait\Firebase\Factory;
+ 
+use Kreait\Firebase\ServiceAccount;
+ 
+use Kreait\Firebase\Database;
+
+class HomeController extends Controller
+{
+public function index(){
+    $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/212feac9ce31.json');
+ 
+    $firebase = (new Factory)
+     
+    ->withServiceAccount($serviceAccount)
+     
+    ->withDatabaseUri('https://tour-app-1084.firebaseio.com/')
+     
+    ->create();
+     
+    $database = $firebase->getDatabase();
+    
+    $reference = $database->getReference('products/today');
+
+    $value = $reference->getValue();
+     
+    //echo"Hello, <pre>";
+     
+    //print_r($value);
+    return view('main',['products' => $value]);
+ 
+}
+
+public function save() {
+    $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/212feac9ce31.json');
+ 
+$firebase = (new Factory)
+ 
+->withServiceAccount($serviceAccount)
+ 
+->withDatabaseUri('https://tour-app-1084.firebaseio.com/')
+ 
+->create();
+ 
+$database = $firebase->getDatabase();
+ 
+$newPost = $database
+ 
+->getReference('products/today')
+ 
+->push([
+ 
+    'title' => 'Sacard Games Part 2',
+     
+    'body' => 'Vikram Chandra',
+    
+    'image' => 'http://every7.xyz/products/wp-content/uploads/2018/09/51ikQ3LtN7L._SX325_BO1204203200_.jpg',
+    
+    'url' => ''
+     
+]);
+ 
+//$newPost->getKey(); // => -KVr5eu8gcTv7_AHb-3-
+ 
+//$newPost->getUri(); // => https://my-project.firebaseio.com/blog/posts/-KVr5eu8gcTv7_AHb-3-
+ 
+//$newPost->getChild('title')->set('Changed post title');
+ 
+//$newPost->getValue(); // Fetches the data from the realtime database
+ 
+//$newPost->remove();
+ 
+echo"<pre>";
+ 
+print_r($newPost->getvalue());
+}
+
+}
+ 
+?>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,254 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <!-- title -->
+        <title>Every7.xyz – New Products @Every 7 Hours</title>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1" />
+        <meta name="author" content="ThemeZaa" />
+        <!-- description -->
+        <meta name="description" content="POFO is a highly creative, modern, visually stunning and Bootstrap responsive multipurpose agency and portfolio HTML5 template with 25 ready home page demos." />
+        <!-- keywords -->
+        <meta name="keywords" content="creative, modern, clean, bootstrap responsive, html5, css3, portfolio, blog, agency, templates, multipurpose, one page, corporate, start-up, studio, branding, designer, freelancer, carousel, parallax, photography, personal, masonry, grid, coming soon, faq" />
+        <!-- favicon -->
+        <link rel="shortcut icon" href="../assets/images/favicon.png" />
+        <link rel="apple-touch-icon" href="../assets/images/apple-touch-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="../assets/images/apple-touch-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="../assets/images/apple-touch-icon-114x114.png" />
+        <!-- animation -->
+        <link rel="stylesheet" href="../assets/css/animate.css" />
+        <!-- bootstrap -->
+        <link rel="stylesheet" href="../assets/css/bootstrap.min.css" />
+        <!-- et line icon --> 
+        <link rel="stylesheet" href="../assets/css/et-line-icons.css" />
+        <!-- font-awesome icon -->
+        <link rel="stylesheet" href="../assets/css/font-awesome.min.css" />
+        <!-- themify icon -->
+        <link rel="stylesheet" href="../assets/css/themify-icons.css" />
+        <!-- swiper carousel -->
+        <link rel="stylesheet" href="../assets/css/swiper.min.css" />
+        <!-- justified gallery  -->
+        <link rel="stylesheet" href="../assets/css/justified-gallery.min.css" />
+        <!-- magnific popup -->
+        <link rel="stylesheet" href="../assets/css/magnific-popup.css" />
+        <!-- revolution slider -->
+        <link rel="stylesheet" type="text/css" href="../assets/revolution/css/settings.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="../assets/revolution/css/layers.css" />
+        <link rel="stylesheet" type="text/css" href="../assets/revolution/css/navigation.css" />
+        <!-- bootsnav -->
+        <link rel="stylesheet" href="../assets/css/bootsnav.css" />
+        <!-- style -->
+        <link rel="stylesheet" href="../assets/css/style.css" />
+        <!-- responsive css -->
+        <link rel="stylesheet" href="../assets/css/responsive.css" />
+        <!--[if IE]>
+            <script src="../assets/js/html5shiv.js"></script>
+        <![endif]-->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body class="width-100">
+        <div class="bg-black position-absolute left-0 top-0 width-100 height-100">
+            <!-- start header -->
+            <header>
+                <!-- start navigation -->
+                <nav class="navbar bg-transparent full-width-pull-menu white-link no-transition">
+                    <div class="container-fluid nav-header-container height-100px padding-three-half-lr xs-height-70px xs-padding-15px-lr">
+                        <!-- start header navigation -->
+                        <div class="row">
+                            <div class="col-md-4 col-sm-4 col-xs-6 hidden-xs text-left">
+                                <div class="header-social-icon border-none no-padding-left no-margin-left">
+                                    <a href="https://fb.me/every7dotcom" title="Facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+                                    <a href="https://twitter.com/" title="Twitter" target="_blank"><i class="fa fa-twitter"></i></a>
+                                    <a href="https://dribbble.com/" title="Dribbble" target="_blank"><i class="fa fa-dribbble"></i></a>                          
+                                </div>
+                            </div>
+                            <!-- start logo -->
+                            <div class="col-md-4 col-sm-4 col-xs-6 text-center xs-text-left">
+                                <a class="logo" href="../assets/index.html"><img src="../assets/images/logo-white.png" data-at2x="images/logo-white@2x.png" alt="pofo" /></a>
+                            </div>
+                            <!-- end logo -->
+                            <div class="col-md-4 col-sm-4 col-xs-6 text-right">
+                                <button class="navbar-toggle mobile-toggle top-0" type="button" id="open-button" data-toggle="collapse" data-target=".navbar-collapse">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </button>
+                                <div class="menu-wrap full-screen no-padding">
+                                    <div class="col-sm-6 no-padding hidden-xs"> 
+                                        <div class="cover-background full-screen">     
+                                            <div class="opacity-light bg-extra-dark-gray"></div>
+                                            <div class="position-absolute height-100 width-100 text-center">
+                                                <div class="display-table height-100 width-100">
+                                                    <div class="display-table-cell height-100 width-100 vertical-align-middle position-relative">
+
+                                                    </div>
+                                                </div>                                                            
+                                            </div>                                                    
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6 no-padding bg-white full-screen text-left">
+                                        <div class="position-absolute height-100 width-100 overflow-auto">
+                                            <div class="display-table height-100 width-100">
+                                                <div class="display-table-cell height-100 width-100 vertical-align-middle padding-fourteen-lr alt-font link-style-2 sm-padding-seven-lr xs-padding-15px-lr">
+                                                    <!-- start menu -->
+                                                    <ul class="font-weight-500 xs-no-padding-left">
+                                                        <!-- start menu item -->
+                                                        <li class="dropdown">
+                                                            <a href="#">Home</a><span data-toggle="dropdown" class="dropdown-toggle"></span>
+                                                        </li>
+                                                        <!-- end menu item -->
+                                                        <!-- start menu item -->
+                                                        <li class="dropdown">
+                                                            <a href="../assets/blog-standard-full-width.html">Blog</a><span data-toggle="dropdown" class="dropdown-toggle"></span>
+                                                        </li>
+                                                        <!-- end menu item -->
+                                                        <!-- start menu item -->
+                                                        <li class="dropdown">
+                                                            <a href="javascript:void(0);">Contact Us</a><span data-toggle="dropdown" class="dropdown-toggle"></span>
+                                                            
+                                                        </li>
+                                                        <!-- end menu item -->
+                                                    </ul>
+                                                    <!-- end menu -->
+                                                    <!-- start social links -->
+                                                    <div class="margin-fifteen-top padding-35px-left xs-no-padding-left">
+                                                        <div class="icon-social-medium margin-three-bottom">
+                                                            <a href="https://fb.me/every7dotcom" target="_blank" class="text-extra-dark-gray text-deep-pink-hover margin-one-lr"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                                            <a href="https://twitter.com/" target="_blank" class="text-extra-dark-gray text-deep-pink-hover margin-one-lr"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                                            <a href="https://dribbble.com/" target="_blank" class="text-extra-dark-gray text-deep-pink-hover margin-one-lr"><i class="fa fa-dribbble" aria-hidden="true"></i></a>
+                                                            <a href="https://plus.google.com" target="_blank" class="text-extra-dark-gray text-deep-pink-hover margin-one-lr"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+                                                            <a href="https://www.tumblr.com/" target="_blank" class="text-extra-dark-gray text-deep-pink-hover margin-one-lr"><i class="fa fa-tumblr" aria-hidden="true"></i></a>
+                                                        </div>
+                                                    </div>
+                                                    <!-- start social links -->
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <button class="close-button-menu" id="close-button"></button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end header navigation -->
+                </nav>
+                <!-- end navigation -->
+            </header>
+            <!-- end header -->
+            <!-- start slider section -->
+            <section class="no-padding">
+                <div class="swiper-bottom-scrollbar-full swiper-container">
+                    <div class="swiper-wrapper">
+                        <!-- start slider item -->
+                        <div class="swiper-slide width-550px xs-width-100 xs-height-auto">
+                            <div class="position-relative width-90 height-100 display-table padding-ten-all xs-padding-fifteen-all xs-width-100">
+                                <div class="display-table-cell vertical-align-middle">
+                                    <h4 class="text-medium-gray font-weight-400 display-block margin-5px-bottom alt-font">Hello,</h4>
+                                    <h6 class="text-medium-gray font-weight-300 margin-20px-bottom alt-font">Welcome to Every7</h6>
+                                    <p class="text-large dispaly-block pull-left font-weight-300 width-90 margin-35px-bottom">You can find new product after every 7 hours. So hurry up an purchase the product before its too late.</p>
+                                    <!-- <img src="../assets/images/signature.png" class="width-60 signature" alt="" /> -->
+                                    
+                                    <!-- @foreach($products as $product)
+                                        {{$product['body']}}
+                                    @endforeach -->
+
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end slider item -->
+                        @foreach($products as $product)
+                        <!-- start slider item -->
+                        <div class="swiper-slide width-auto xs-height-auto last-paragraph-no-margin">
+                            <div class="height-100 display-table">
+                                <div class="display-table-cell vertical-align-middle">
+                                    <div class="display-block position-relative">
+                                        <img src="{{$product['image']}}" alt="" />
+                                        <p class="bottom-text width-100 text-extra-small text-white text-uppercase text-center">{{$product['body']}}</p>
+                                    </div>
+                                    <div class="hover-title-box padding-55px-lr width-300px sm-width-100 sm-padding-20px-lr">
+                                        <div class="separator width-50px bg-black md-display-none xs-margin-lr-auto"></div>
+                                        <h3><a class="text-white font-weight-500 alt-font text-white-hover" href="../assets/single-project-page-01.html">{{$product['title']}}</a></h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end slider item -->
+                        @endforeach
+                        
+                        <div class="swiper-slide width-150px xs-width-100 xs-height-auto">
+                        </div>
+                    </div>
+                    <!-- start slider swiper pagination -->
+                    <div class="swiper-scrollbar xs-display-none"></div>
+                    <div class="swiper-pagination-vertical position-fixed bullet-deep-pink z-index-5"></div>
+                    <!-- end slider swiper pagination -->
+                </div>
+            </section>
+            <!-- end slider section -->
+        </div>
+        <!-- start scroll to top -->
+        <a class="scroll-top-arrow" href="javascript:void(0);"><i class="ti-arrow-up"></i></a>
+        <!-- end scroll to top  -->
+        <!-- javascript libraries -->
+        <script type="text/javascript" src="../assets/js/jquery.js"></script>
+        <script type="text/javascript" src="../assets/js/modernizr.js"></script>
+        <script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="../assets/js/jquery.easing.1.3.js"></script>
+        <script type="text/javascript" src="../assets/js/skrollr.min.js"></script>
+        <script type="text/javascript" src="../assets/js/smooth-scroll.js"></script>
+        <script type="text/javascript" src="../assets/js/jquery.appear.js"></script>
+        <!-- menu navigation -->
+        <script type="text/javascript" src="../assets/js/bootsnav.js"></script>
+        <script type="text/javascript" src="../assets/js/jquery.nav.js"></script>
+        <!-- animation -->
+        <script type="text/javascript" src="../assets/js/wow.min.js"></script>
+        <!-- page scroll -->
+        <script type="text/javascript" src="../assets/js/page-scroll.js"></script>
+        <!-- swiper carousel -->
+        <script type="text/javascript" src="../assets/js/swiper.min.js"></script>
+        <!-- counter -->
+        <script type="text/javascript" src="../assets/js/jquery.count-to.js"></script>
+        <!-- parallax -->
+        <script type="text/javascript" src="../assets/js/jquery.stellar.js"></script>
+        <!-- magnific popup -->
+        <script type="text/javascript" src="../assets/js/jquery.magnific-popup.min.js"></script>
+        <!-- portfolio with shorting tab -->
+        <script type="text/javascript" src="../assets/js/isotope.pkgd.min.js"></script>
+        <!-- images loaded -->
+        <script type="text/javascript" src="../assets/js/imagesloaded.pkgd.min.js"></script>
+        <!-- pull menu -->
+        <script type="text/javascript" src="../assets/js/classie.js"></script>
+        <script type="text/javascript" src="../assets/js/hamburger-menu.js"></script>
+        <!-- counter  -->
+        <script type="text/javascript" src="../assets/js/counter.js"></script>
+        <!-- fit video  -->
+        <script type="text/javascript" src="../assets/js/jquery.fitvids.js"></script>
+        <!-- equalize -->
+        <script type="text/javascript" src="../assets/js/equalize.min.js"></script>
+        <!-- skill bars  -->
+        <script type="text/javascript" src="../assets/js/skill.bars.jquery.js"></script> 
+        <!-- justified gallery  -->
+        <script type="text/javascript" src="../assets/js/justified-gallery.min.js"></script>
+        <!--pie chart-->
+        <script type="text/javascript" src="../assets/js/jquery.easypiechart.min.js"></script>
+        <!-- instagram -->
+        <script type="text/javascript" src="../assets/js/instafeed.min.js"></script>
+        <!-- retina -->
+        <script type="text/javascript" src="../assets/js/retina.min.js"></script>
+        <!-- revolution -->
+        <script type="text/javascript" src="../assets/revolution/js/jquery.themepunch.tools.min.js"></script>
+        <script type="text/javascript" src="../assets/revolution/js/jquery.themepunch.revolution.min.js"></script>
+        <!-- revolution slider extensions (load below extensions JS files only on local file systems to make the slider work! The following part can be removed on server for on demand loading) -->
+        <!--<script type="text/javascript" src="revolution/js/extensions/revolution.extension.actions.min.js"></script>
+        <script type="text/javascript" src="revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+        <script type="text/javascript" src="revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+        <script type="text/javascript" src="revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+        <script type="text/javascript" src="revolution/js/extensions/revolution.extension.migration.min.js"></script>
+        <script type="text/javascript" src="revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+        <script type="text/javascript" src="revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+        <script type="text/javascript" src="revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+        <script type="text/javascript" src="revolution/js/extensions/revolution.extension.video.min.js"></script>-->
+        <!-- setting -->
+        <script type="text/javascript" src="../assets/js/main.js"></script>
+    </body>
+</html>

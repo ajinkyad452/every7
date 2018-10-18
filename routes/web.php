932d75b4bed9f207e126a771e4bc1114161291bc
Ajinkya <dube.ajinkya452@gmail.com>
2018-10-18 20:59:37 +0530

    added home root

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,11 @@
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
+
+Route::get('/','HomeController@index');
 
 // Route::get('/every7', function () {
 //     return view('main');
